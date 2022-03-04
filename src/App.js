56e0183be6f89fa393d8621da84b36c2411e5f67
booennm <booennm@gmail.com>
2022-03-04 20:33:55 +0200

    add is functional

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 const URL = 'https://localhost/shoppinglist/';
 
 function App() {
+  const [itemDesc, setItemDesc] = useState('');
+  const [itemAmount, setItemAmount] = useState(0);
   const [items, setItems] = useState([]);
 
   useEffect(() => {
@@ -16,12 +18,33 @@ function App() {
       })
   }, [])
 
+  function add(e) {
+    e.preventDefault();
+    const json = JSON.stringify({description:itemDesc, amount:itemAmount});
+    axios.post(URL + 'add.php', json, {
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) => {
+      setItems(items => [...items, response.data]);
+    }).catch(error => {
+      alert(error.response ? error.response.data.error : error);
+    })
+  }
+
   return (
     <div>
+      <form onSubmit={add}>
+        <label>Item description </label>
+        <input value={itemDesc} onChange={e => setItemDesc(e.target.value)}></input>
+        <input type='number' value={itemAmount} onChange={e => setItemAmount(e.target.value)}></input>
+        <button>Add</button>
+      </form>
       <ol>
         {items.map(item => (
           <li key={item.id}>
-            {item.description} {item.amount}
+            {item.description}, {item.amount}kpl
           </li>
         ))}
       </ol>
