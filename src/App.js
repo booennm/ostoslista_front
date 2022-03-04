3533b6faee2ea9983426c41093bf7b0f47d3d54b
booennm <booennm@gmail.com>
2022-03-04 18:49:22 +0200

    mapping fetched data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const URL = 'https://localhost/shoppinglist/';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        setItems(response.data);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      })
+  }, [])
+
   return (
-    
+    <div>
+      <ol>
+        {items.map(item => (
+          <li key={item.id}>
+            {item.description} {item.amount}
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 }
 
