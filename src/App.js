139f3dff07ec2a7864ebada2fa679e880f5458c1
booennm <booennm@gmail.com>
2022-03-04 20:58:41 +0200

    remove is functional

--- src/App.js
+++ src/App.js
@@ -33,18 +33,36 @@ function App() {
     })
   }
 
+  function remove(id) {
+    const json = JSON.stringify({id:id})
+    axios.post(URL + 'remove.php', json, {
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) => {
+      const tempList = items.filter((item) => item.id !== id);
+      setItems(tempList);
+    }).catch (error => {
+      alert(error.response ? error.response.data.error : error);
+    })
+  }
+
   return (
     <div>
       <form onSubmit={add}>
+        <p>Add item</p>
         <label>Item description </label>
         <input value={itemDesc} onChange={e => setItemDesc(e.target.value)}></input>
         <input type='number' value={itemAmount} onChange={e => setItemAmount(e.target.value)}></input>
         <button>Add</button>
       </form>
+      <p>Shopping list</p>
       <ol>
         {items.map(item => (
           <li key={item.id}>
             {item.description}, {item.amount}kpl
+            <a href='#' className='remove' onClick={() => remove(item.id)}>remove</a>
           </li>
         ))}
       </ol>
